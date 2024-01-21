f3fbc4248f27bb1994c3b9cac8fff000ae64c638
cupich <ilya_naumov_2015@list.ru>
2024-01-21 16:27:50 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='block'>
+          <p>
+              123
+          </p>
+      </div>
+      <div className='block'>
+          <p>
+              123
+          </p>
+      </div>
+      <div className='block'>
+          <p>
+              123
+          </p>
+      </div>
+      <div className='block'>
+          <p>
+              123
+          </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
